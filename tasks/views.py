0d6d9a58d233972e816227374e2fda864a40a9e5
Alexandr Politov <apolitov83@gmail.com>
2024-09-25 12:40:50 +0300

    task update/delete: buttons added to task-list page, views, urls, htmls created

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,28 @@
+from audioop import reverse
+
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views import generic
 
 from tasks.models import Task
 
 
-class TaskListView(ListView):
+class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
