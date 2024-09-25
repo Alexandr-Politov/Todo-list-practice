dccda8c364a81de8061a67e9216025a520131911
Alexandr Politov <apolitov83@gmail.com>
2024-09-25 11:08:36 +0300

    models (task, tag) created. Migrations done.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
